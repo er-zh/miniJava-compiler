86f9d6430f06ef9a82104b8c4beeed98604390b9
ezheng118 <ezheng118@gmail.com>
2021-02-02 00:06:40 -0500

    initial Token class and TokenType enum created

--- src/miniJava/SyntacticAnalyzer/Token.java
+++ src/miniJava/SyntacticAnalyzer/Token.java
@@ -1,5 +1,19 @@
 package miniJava.SyntacticAnalyzer;
 
 public class Token {
-
+	private TokenType type;
+	private String lexeme;
+	
+	public Token(TokenType t, String text) {
+		type = t;
+		lexeme = text;
+	}
+	
+	public TokenType getType() {
+		return type;
+	}
+	
+	public String getLexeme() {
+		return lexeme;
+	}
 }
---
+++ src/miniJava/SyntacticAnalyzer/TokenType.java
@@ -0,0 +1,39 @@
+package miniJava.SyntacticAnalyzer;
+
+public enum TokenType {
+	EOT,
+	//punctuation
+		// = sign
+		ASSIGNMENT,
+		// delimiters
+		LPAREN,
+		RPAREN,
+		LBRACE,
+		RBRACE,
+		LSQUARE,
+		RSQUARE,
+		SEMICOLON,
+		COMMA,
+	//keywords
+		CLASS,
+		// control structures
+		IF,
+		ELSE,
+		WHILE,
+		RETURN,
+		// visibility
+		PUBLIC,
+		PRIVATE,
+		// scope related
+		STATIC,
+		THIS,
+		// primitive types
+		INT,
+		BOOLEAN, T, F,
+		VOID,
+	// sets of terminals, arbitrarily identified
+	ID, //name
+	NUM_LITERAL,
+	UNOP, //punctuation
+	BINOP	
+}
