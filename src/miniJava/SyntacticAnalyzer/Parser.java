1d485a94fae725f46e9b205d22f65f3cdbb22fe8
ezheng118 <ezheng118@gmail.com>
2021-02-06 16:58:09 -0500

    parseStatement now implemented

--- src/miniJava/SyntacticAnalyzer/Parser.java
+++ src/miniJava/SyntacticAnalyzer/Parser.java
@@ -15,7 +15,7 @@ public class Parser {
 			parseProgram();
 		}
 		catch(SyntaxError e) {
-			
+			e.printErrorMsg();
 		}
 	}
 	
@@ -28,43 +28,222 @@ public class Parser {
 	}
 	
 	void parseClassDecl() {
+		accept(TokenType.CLASS);
+		accept(TokenType.ID);
+		accept(TokenType.LBRACE);
 		
+		parseVis();
+		parseAccess();
+		
+		if(currentToken.getType() == TokenType.VOID) {
+			accept(TokenType.VOID);
+		}
+		else {
+			parseType();
+		}
 	}
 	
 	void parseFieldDecl() {
-		
+		accept(TokenType.SEMICOLON);
 	}
 	
-	void parseMethoDecl() {
+	void parseMethodDecl() {
+		accept(TokenType.LPAREN);
 		
+		if(currentToken.getType() != TokenType.RPAREN) {
+			parseParamList();
+		}
+		
+		accept(TokenType.RPAREN);
+		accept(TokenType.LBRACE);
+		
+		while(currentToken.getType() != TokenType.RBRACE) {
+			parseStatement();
+		}
+		
+		accept(TokenType.RBRACE);
 	}
 	
+	// can be empty
 	void parseVis() {
-		
+		if(currentToken.getType() == TokenType.PRIVATE) {
+			advance();
+		}
+		else if(currentToken.getType() == TokenType.PUBLIC) {
+			advance();
+		}
 	}
 	
-	void parseAccess() {
-		
+	// can be empty
+	void parseAccess() throws SyntaxError {
+		if(currentToken.getType() == TokenType.STATIC) {
+			advance();
+		}
 	}
 	
 	void parseType() {
-		
+		if(currentToken.getType() == TokenType.BOOLEAN) {
+			advance();
+		}
+		else {
+			if(currentToken.getType() == TokenType.INT || currentToken.getType() == TokenType.ID) {
+				advance();
+				if(currentToken.getType() == TokenType.LSQUARE) {
+					advance();
+					accept(TokenType.RSQUARE);
+				}
+			}
+			else {
+				throw new SyntaxError("incorrect typing");
+			}
+		}
 	}
 	
+	// can be empty
 	void parseParamList() {
+		parseType();
+		accept(TokenType.ID);
 		
+		while(currentToken.getType() == TokenType.COMMA) {
+			advance();
+			parseType();
+			accept(TokenType.ID);
+		}
 	}
 	
+	// can be empty
 	void parseArgList() {
+		parseExpr();
 		
+		while(currentToken.getType() == TokenType.COMMA) {
+			advance();
+			parseExpr();
+		}
 	}
 	
 	void parseRef() {
+		if(currentToken.getType() == TokenType.ID || currentToken.getType() == TokenType.THIS) {
+			advance();
+			
+			while(currentToken.getType() == TokenType.PERIOD) {
+				advance();
+				accept(TokenType.ID);
+			}
+		}
 		
 	}
 	
 	void parseStatement() {
-		
+		switch(currentToken.getType()) {
+		case LBRACE:
+			advance();
+			while(currentToken.getType() != TokenType.RBRACE) {
+				parseStatement();
+			}
+			accept(TokenType.RBRACE);
+			break;
+		case IF:
+			advance();
+			accept(TokenType.LPAREN);
+			parseExpr();
+			accept(TokenType.RPAREN);
+			
+			parseStatement();
+			
+			if(currentToken.getType() == TokenType.ELSE) {
+				advance();
+				parseStatement();
+			}
+			
+		case WHILE:
+			advance();
+			accept(TokenType.LPAREN);
+			parseExpr();
+			accept(TokenType.RPAREN);
+			
+			parseStatement();
+			break;
+		case RETURN:
+			advance();
+			if(currentToken.getType() != TokenType.SEMICOLON) {
+				parseExpr();
+			}
+			accept(TokenType.SEMICOLON);
+			break;
+		case ID:
+			// need to decide between 
+			// type(id) id = expr;
+			// and 
+			// ref (some kind of expr);
+			// which requires left factoring of possible id
+			advance();
+			
+			if(currentToken.getType() == TokenType.PERIOD) {
+				// remaining part of parsing a ref
+				while(currentToken.getType() == TokenType.PERIOD) {
+					advance();
+					accept(TokenType.ID);
+				}
+				
+				parseStatementRef();
+			}
+			else {
+				// remaining portion of parsing a type
+				if(currentToken.getType() == TokenType.LSQUARE) {
+					advance();
+					accept(TokenType.RSQUARE);
+				}
+				parseStatementAssign();
+			}
+			break;
+		case THIS:
+			parseRef();
+			
+			parseStatementRef();
+			break;
+		case BOOLEAN:
+		case INT:
+			parseType();
+			parseStatementAssign();
+			break;
+		default:
+			throw new SyntaxError("failed to parse statement");
+		}
+	}
+	
+	private void parseStatementRef() {
+		switch(currentToken.getType()) {
+		case ASSIGNMENT:
+			advance();
+			parseExpr();
+			break;
+		case LSQUARE:
+			advance();
+			parseExpr();
+			accept(TokenType.RSQUARE);
+			accept(TokenType.ASSIGNMENT);
+			parseExpr();
+			break;
+		case LPAREN:
+			advance();
+			
+			if(currentToken.getType() != TokenType.RPAREN) {
+				parseArgList();
+			}
+			
+			accept(TokenType.RPAREN);
+			break;
+		default:
+			throw new SyntaxError("failed to parse ref statement");
+		}
+		accept(TokenType.SEMICOLON);
+	}
+	
+	private void parseStatementAssign() {
+		accept(TokenType.ID);
+		accept(TokenType.ASSIGNMENT);
+		parseExpr();
+		accept(TokenType.SEMICOLON);
 	}
 	
 	void parseExpr() {
@@ -75,10 +254,18 @@ public class Parser {
 		if (currentToken.getType() == expectedToken) {
 			/*if (trace)
 				pTrace();*/
-			currentToken = scanner.getNextToken();
+			advance();
 		}
 		else {
 			throw new SyntaxError(expectedToken, currentToken.getType());
+		}
+	}
+	
+	private void advance() {
+		currentToken = scanner.getNextToken();
+		
+		if(currentToken.getType() == TokenType.ERROR) {
+			throw new SyntaxError("Lexical error, " + currentToken.getLexeme());
 		}
 	}
 		
--- src/miniJava/SyntacticAnalyzer/SyntaxError.java
+++ src/miniJava/SyntacticAnalyzer/SyntaxError.java
@@ -5,14 +5,27 @@ public class SyntaxError extends Error{
 	
 	private TokenType expected;
 	private TokenType recieved;
+	private String errorMsg;
 	
 	public SyntaxError(TokenType expect, TokenType found) {
 		expected = expect;
 		recieved = found;
+		
+		errorMsg = "expecting '" + expected + "' but found '" + recieved + "'";
+	}
+	
+	public SyntaxError(String error) {
+		errorMsg = error;
+	}
+	
+	public SyntaxError(TokenType expect, TokenType found, String error) {
+		expected = expect;
+		recieved = found;
+		
+		errorMsg = error;
 	}
 	
 	public void printErrorMsg() {
-		System.out.println("expecting '" + expected +
-				"' but found '" + recieved + "'");
+		System.out.println(errorMsg);
 	}
 }
